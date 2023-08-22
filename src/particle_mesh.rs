0d84d07aebeeddc5c88755fbe6ec4320a0a4abfe
bhargav <bhargav.annem@gmail.com>
2023-08-22 18:12:47 -0500

    feat: plot distributions

--- src/particle_mesh.rs
+++ src/particle_mesh.rs
@@ -36,13 +36,15 @@ mod tests {
         let average_density = (N_CELLS / N_PARTICLES).pow(3) as f64;
         let rho: Array3<f64> = gaussian_random_field(0.845, 3.685);
         let (positions, velocities): (Array2<f64>, Array2<f64>) = initial_conditions(rho.clone());
-        let a = positions
-            .slice(s![0, ..])
-            .to_vec()
-            .iter()
-            .map(|x| *x as u32)
-            .collect::<Vec<u32>>();
-        println!("{:?}", positions.shape());
-        hist(a, Some("x_distribution".to_string()));
+        ['x', 'y', 'z'].iter().enumerate().for_each(|(dir, c)| {
+            let a = positions
+                .slice(s![dir, ..])
+                .to_vec()
+                .iter()
+                .map(|x| *x as u32)
+                .collect::<Vec<u32>>();
+            println!("{:?}", positions.shape());
+            hist(a, Some(format!("{}_distribution", c)));
+        })
     }
 }
